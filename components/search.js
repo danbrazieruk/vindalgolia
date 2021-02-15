507534078e7039a9ee6ca30e1bdde653d5981dcc
Dan Brazier <dan@danbrazier.co.uk>
2021-02-15 12:02:34 +0000

    filters added, configured replica indices for sortby

--- components/search.js
+++ components/search.js
@@ -2,15 +2,16 @@
 import algoliasearch from 'algoliasearch/lite'
 import {
   InstantSearch,
+  SearchBox,
   Hits,
   HitsPerPage,
-  SearchBox,
   Pagination,
   Highlight,
   ClearRefinements,
   RefinementList,
   Configure,
   SortBy,
+  Panel,
 } from 'react-instantsearch-dom';
 
 
@@ -26,7 +27,7 @@ function Search() {
     return (
         <InstantSearch searchClient={searchClient} indexName="bordeaux">
 
-            <Header />
+            <Header/>
 
             <Content/>
 
@@ -49,7 +50,7 @@ const Header = () => (
 
       <SearchBox
           className="search-bar"
-          translations={{ placeholder: 'Search for Wine' }}
+          translations={{ placeholder: 'Search Wines' }}
       />
     </header>
 );
@@ -64,7 +65,7 @@ const Hit = ({ hit }) => (
     </div>
 
     <div className="card-body">
-        <p className="small uppercase">{hit.domain}</p>
+        <p className="small uppercase letter-spacing">{hit.domain}</p>
         <p className="card-title">{hit.name} <span className="card-year">{hit.year}</span></p>
 
         <div className="card-price" hit={hit}>€ {hit.price}</div>
@@ -76,78 +77,150 @@ const Hit = ({ hit }) => (
 
 {/* Content */}
 const Content = () => (
-    <main className="main">
-      <div className="container">
+  <main className="main">
 
-      <HitsPerPage
-            className="container-option"
-            items={[
-              {
-                label: '12 hits per page',
-                value: 12,
-              },
-              {
-                label: '18 hits per page',
-                value: 18,
-              },
-              {
-                label: '24 hits per page',
-                value: 24,
-              },
-            ]}
-            defaultRefinement={12}
-          />
+    {/* Filters */}
+    <div className="container container--filters">
+      <h3>Filters</h3>
 
-        <Hits hitComponent = {Hit} />
+      <div className="clear-filters">
+        <ClearRefinements
+          translations={{
+            reset: (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="11"
+                  height="11"
+                  viewBox="0 0 11 11"
+                >
+                  <g fill="none" fillRule="evenodd" opacity=".4">
+                    <path d="M0 0h11v11H0z" />
+                    <path
+                      fill="#000"
+                      fillRule="nonzero"
+                      d="M8.26 2.75a3.896 3.896 0 1 0 1.102 3.262l.007-.056a.49.49 0 0 1 .485-.456c.253 0 .451.206.437.457 0 0 .012-.109-.006.061a4.813 4.813 0 1 1-1.348-3.887v-.987a.458.458 0 1 1 .917.002v2.062a.459.459 0 0 1-.459.459H7.334a.458.458 0 1 1-.002-.917h.928z"
+                    />
+                  </g>
+                </svg>
+                Clear filters
+              </>
+            ),
+          }}
+        />
       </div>
 
-      <Pagination
-              padding={2}
-              showFirst={false}
-              showLast={false}
-              translations={{
-                previous: (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                  >
-                    <g
-                      fill="none"
-                      fillRule="evenodd"
-                      stroke="#000"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.143"
-                    >
-                      <path d="M9 5H1M5 9L1 5l4-4" />
-                    </g>
-                  </svg>
-                ),
-                next: (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                  >
-                    <g
-                      fill="none"
-                      fillRule="evenodd"
-                      stroke="#000"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.143"
-                    >
-                      <path d="M1 5h8M5 9l4-4-4-4" />
-                    </g>
-                  </svg>
-                ),
-              }}
-            />
+      <div className="">
+        <Panel className="small uppercase letter-spacing" header="Region">
+          <RefinementList
+            attribute="domain"
+            searchable={true}
+            translations={{
+              placeholder: 'Search Regions…',
+            }}
+          />
+        </Panel>
+      </div>
+    </div>
 
-    </main>
+
+    {/* Results */}
+    <div className="container container--results">
+
+      <SortBy
+        className="container-option"
+        defaultRefinement="price-desc"
+        items={[
+          {
+            label: 'Price (low to high))',
+            value: 'price-asc',
+          },
+          {
+            label: 'Price (high to low)',
+            value: 'price-desc',
+          },
+          {
+            label: 'Year (low to high))',
+            value: 'year-asc',
+          },
+          {
+            label: 'Year (high to low)',
+            value: 'year-desc',
+          },
+        ]}
+      />
+
+      <HitsPerPage
+        className="container-option"
+        items={[
+          {
+            label: '12 items per page',
+            value: 12,
+          },
+          {
+            label: '18 items per page',
+            value: 18,
+          },
+          {
+            label: '24 items per page',
+            value: 24,
+          },
+        ]}
+        defaultRefinement={12}
+      />
+
+      <Hits hitComponent = {Hit} />
+
+      <Pagination
+        padding={2}
+        showFirst={false}
+        showLast={false}
+        translations={{
+          previous: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="10"
+              viewBox="0 0 10 10"
+            >
+              <g
+                fill="none"
+                fillRule="evenodd"
+                stroke="#000"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.143"
+              >
+                <path d="M9 5H1M5 9L1 5l4-4" />
+              </g>
+            </svg>
+          ),
+
+          next: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="10"
+              viewBox="0 0 10 10"
+            >
+              <g
+                fill="none"
+                fillRule="evenodd"
+                stroke="#000"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.143"
+              >
+                <path d="M1 5h8M5 9l4-4-4-4" />
+              </g>
+            </svg>
+          ),
+        }}
+      />
+
+    </div>
+
+  </main>
 );
 
 export default Search;
